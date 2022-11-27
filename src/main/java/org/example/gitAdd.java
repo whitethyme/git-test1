37f73ed539ca8267d71f48b1aacf56d76153f413
lvhui <whitethyme77@gmail.com>
2022-11-27 12:05:49 +0800

    version5.0.0

--- src/main/java/org/example/gitAdd.java
+++ src/main/java/org/example/gitAdd.java
@@ -9,5 +9,7 @@ public class gitAdd
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        System.out.println("hello idea-git-gitee!");
     }
 }
